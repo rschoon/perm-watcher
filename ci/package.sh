7d17940ab1747c3441c9b34f5fe321df8ac5294a
Robin Schoonover <robin@cornhooves.org>
2020-02-20 18:04:52 -0700

    Add CI build script

---
+++ ci/package.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -x
+
+cd `dirname $0`/..
+
+WORKDIR=target/pkg
+PKG=perm-watcher
+
+if [ ! -z "$VERSION" ]; then
+    VERSION="-$VERSION"
+fi
+
+mkdir -p dist $WORKDIR/$PKG
+
+cp target/release/perm-watcher $WORKDIR/$PKG/
+
+tar -cjf dist/perm-watcher$VERSION.tar.bz2 -C $WORKDIR $PKG
+
+rm -r $WORKDIR
+
