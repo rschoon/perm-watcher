8077d3a2c2749635f55c7fc2fc2b4a1102c08529
Robin Schoonover <robin@cornhooves.org>
2021-02-13 15:17:34 -0700

    Actual build debian package

--- ci/package.sh
+++ ci/package.sh
@@ -5,15 +5,23 @@ cd `dirname $0`/..
 WORKDIR=target/pkg
 PKG=perm-watcher
 
+if [ -z "$CI_COMMIT_TAG" ] || [ ! -z "$DEV_SNAPSHOT" ]; then
+    REL=`date +"%Y%m%d%H%M%S"`
+    VERSION=`cargo metadata --format-version 1 | jq -r '.packages[] | select(.name=="perm-watcher") | .version'`
+    DEB_ARGS="--deb-version $VERSION-0.$REL"
+fi
+
+cargo deb $DEB_ARGS
+
 if [ ! -z "$VERSION" ]; then
-    VERSION="-$VERSION"
+    VERSION_SUFFIX="-$VERSION"
 fi
 
 mkdir -p dist $WORKDIR/$PKG
 
+cp target/debian/*.deb dist/
 cp target/release/perm-watcher $WORKDIR/$PKG/
-
-tar -cjf dist/perm-watcher-bin$VERSION.tar.bz2 -C $WORKDIR $PKG
+tar -cjf dist/perm-watcher-bin$VERSION_SUFFIX.tar.bz2 -C $WORKDIR $PKG
 
 rm -r $WORKDIR
 
